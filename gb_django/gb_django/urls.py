2d71d70c0be0cbb3b176b783e7200ce4363e7c3d
nmaz <mazurov@maz.maz>
2023-03-05 23:45:26 +0300

    change  urls and view

--- gb_django/gb_django/urls.py
+++ gb_django/gb_django/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from mainapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('', views.hello_world, name='hello_world'),
+
+    path('blog',views.blog),
 ]
